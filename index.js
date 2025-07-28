8b859801b0707ec7f34b6237ac6ba007cce88d1d
Priyesh Dwivedi <priyesh88088@gmail.com>
2025-07-28 12:13:45 +0530

    gitfileadded

--- index.js
+++ index.js
@@ -4,9 +4,13 @@ const jobRoute= require("./route/job.route") ;
 
 const mongoose=require("mongoose");
 
+const dotenv = require("dotenv");
+ dotenv.config();
+
 const app=express();
 
 app.use(express.json());
+console.log(process.env.db_Url);     
 mongoose.connect("mongodb+srv://priyesh88088:3c7sKd7ej76YWrR8@cluster0.efm2ruf.mongodb.net/").
 then(()=>{console.log("Connected to MongoDB")})
 .catch((err)=>{console.log("Error in connecting",err)})
